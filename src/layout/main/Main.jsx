4fa897f7f9aee60fd5931e0b87c9b14908efbc02
Denis Agureev <dkihada@gmail.com>
2021-08-30 23:33:24 +0500

    main refactor via hooks

--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -1,35 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Movies } from "../../components/Movies/Movies";
 import { Preloader } from "../../components/Preloader/Preloader";
 import { Search } from "../../components/Search/Search";
 import "./Main.css";
 
-class Main extends React.Component {
-  state = {
-    movies: [],
-    loading: true,
-  };
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch(
-      "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-API-KEY": "4fa00bd4-9132-4722-b0d3-5ea6515ac863",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.films, loading: false }))
-      .catch((err) => {
-        console.log(err);
-        this.setState({ loading: false });
-      });
-  }
-
-  searchMovie = (str) => {
-    this.setState({ loading: true });
+  const searchMovie = (str) => {
+    setLoading(true);
 
     fetch(
       `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${str}`,
@@ -42,24 +22,42 @@ class Main extends React.Component {
     )
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ movies: data.films, loading: false });
-        console.log(data);
+        setMovies(data.films);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        this.setState({ loading: false });
+        setLoading(false);
       });
   };
 
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <main className='container content'>
-        <Search searchMovie={this.searchMovie} />
-        {loading ? <Preloader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
+  useEffect(() => {
+    fetch(
+      "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": "4fa00bd4-9132-4722-b0d3-5ea6515ac863",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.films);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className='container content'>
+      <Search searchMovie={searchMovie} />
+      {loading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
 }
 
 export { Main };
