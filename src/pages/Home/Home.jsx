196a9b160a2232b971e5b84283eae7fd26321984
dkihada <dkihada@gmail.com>
2021-10-19 02:00:54 +0500

    create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+import { Movies } from '../../components/Movies/Movies';
+import { Preloader } from '../../components/Preloader/Preloader';
+import { Sidebar } from '../../layout/sidebar/Sidebar';
+import { StateContext } from '../../services/Context';
+import styles from './Home.module.css';
+
+function Home() {
+	const { loading, search, movies } = useContext(StateContext);
+
+	console.log();
+
+	return (
+		<>
+			<div className='sidebar'>
+				<Sidebar />
+				{/* <Pagination typeMovies={type._top_100} /> */}
+			</div>
+
+			{movies.length && search ? (
+				<>
+					<Movies />
+				</>
+			) : (
+				<div className={styles.body}>
+					<h1>Доброе пожаловать на MoviesHub! </h1>
+					<p>
+						Выберите из списка меню категорию фильмов или введите в поиске
+						интересующий вас фильм.
+					</p>
+				</div>
+			)}
+		</>
+	);
+}
+
+export { Home };
