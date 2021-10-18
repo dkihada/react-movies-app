9fa0d8a2650a95d3a8d891aead598efa323384c8
dkihada <dkihada@gmail.com>
2021-10-18 19:06:13 +0500

    add Logo

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import { Menu } from '../../components/Menu/Menu';
 import { Search } from '../../components/Search/Search';
+import { Logo } from '../../components/Logo/Logo';
 
 function Sidebar() {
 	return (
 		<div className='sidebar'>
 			<div>
+				<Logo />
 				<Search />
 				<Menu />
 			</div>
