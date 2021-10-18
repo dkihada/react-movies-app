7b5ea8c3fae47f8011b4353058b542c08b634b54
dkihada <dkihada@gmail.com>
2021-10-18 19:00:13 +0500

    add Pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,32 +1,46 @@
 import { useContext } from 'react';
-import { getTopMovies } from '../../services/API';
+import { getTopMovies, searchList } from '../../services/API';
 import styles from './Pagination.module.css';
+import './Pagination.css';
 import { StateContext } from '../../services/Context';
 import ReactPaginate from 'react-paginate';
 
 function Pagination({ typeMovies }) {
-	const { pages, getMovies } = useContext(StateContext);
+	const { pages, getMovies, search, getPages } = useContext(StateContext);
 	return (
-		<ReactPaginate
-			pageCount={pages}
-			pageRangeDisplayed={1}
-			marginPagesDisplayed={3}
-			previousLabel={'<'}
-			nextLabel={'>'}
-			containerClassName={styles.pagination}
-			activeClassName={styles.selected}
-			pageClassName={styles.li}
-			forcePage={0}
-			onPageChange={(e) => {
-				getTopMovies(typeMovies, e.selected + 1)
-					.then((data) => {
-						getMovies(data.films);
-					})
-					.catch((err) => {
-						console.log(err);
-					});
-			}}
-		/>
+		<>
+			{pages > 1 ? (
+				<ReactPaginate
+					pageCount={pages}
+					pageRangeDisplayed={1}
+					marginPagesDisplayed={3}
+					previousLabel={'<'}
+					nextLabel={'>'}
+					containerClassName={styles.pagination}
+					activeClassName={styles.selected}
+					pageClassName={styles.li}
+					forcePage={0}
+					onPageChange={(e) => {
+						!search
+							? getTopMovies(typeMovies, e.selected + 1)
+									.then((data) => {
+										getMovies(data.films);
+									})
+									.catch((err) => {
+										console.log(err);
+									})
+							: searchList(search, e.selected + 1)
+									.then((data) => {
+										getMovies(data.films);
+										getPages(data.pagesCount);
+									})
+									.catch((err) => {
+										console.log(err);
+									});
+					}}
+				/>
+			) : null}
+		</>
 	);
 }
 
